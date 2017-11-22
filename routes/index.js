9c4cff9846c2d37b9e169634148f4ed0f6f762ca
Dharmang Hansaliya <dhansaliya@hunterconsulting.com>
2017-11-22 07:56:37 -0500

    Can save a Array.

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,52 @@ const User = require('../models/user');
 
 /* Save Data using Post Request. */
 router.post('/savePost', function (req, res) {
-    if (!req.body.imei) {
-        res.json({succes: false, message: 'IMEI number not provided'})
-    } else if (!req.body.appID) {
-        res.json({success: false, message: 'Application ID not provided'})
-    } else if (!req.body.appName) {
-        res.json({success: false, message: 'Application Name not provided.'})
+
+    let err_msg = [];
+
+    for (let i = 0, len = req.body.length; i < len; i++) {
+        if (!req.body[i].imei) {
+            err_msg.push({succes: false, message: 'IMEI number not provided. ' + i})
+        } else if (!req.body[i].packageName) {
+            err_msg.push({success: false, message: 'Application ID not provided in array. ' + i})
+        } else if (!req.body[i].appName) {
+            err_msg.push({success: false, message: 'Application Name not provided in array. ' + i})
+        }
+    }
+
+    if (err_msg.length !== 0) {
+        res.json(err_msg);
     } else {
-        let user = new User({
-            imei: req.body.imei,
-            appID: req.body.appID,
-            appName: req.body.appName
+        req.body.forEach((arr) => {
+            let user = new User({
+                imei: arr.imei,
+                appID: arr.packageName,
+                appName: arr.appName
+            });
+
+            user.save((err) => {
+                if (err) {
+                    res.json({success: false, message: err})
+                }
+            })
         });
 
-        user.save((err) => {
-            if (err) {
-                res.json({success: false, message: err})
-            } else {
-                res.json({success: 'true', message: 'Data Saved'})
-            }
-        })
+        res.json({success: true, message: 'All data saved.'})
     }
 });
 
 /* Save Data using Get Request */
-router.get('/saveGet/:imei/:appID/:appName', function (req, res) {
+router.get('/saveGet/:imei/:packageName/:appName', function (req, res) {
     if (!req.params.imei) {
         res.json({succes: false, message: 'IMEI number not provided'})
-    } else if (!req.params.appID) {
+    } else if (!req.params.packageName) {
         res.json({success: false, message: 'Application ID not provided'})
     } else if (!req.params.appName) {
         res.json({success: false, message: 'Application Name not provided.'})
     } else {
         let user = new User({
             imei: req.params.imei,
-            appID: req.params.appID,
+            packageName: req.params.packageName,
             appName: req.params.appName
         });
 
